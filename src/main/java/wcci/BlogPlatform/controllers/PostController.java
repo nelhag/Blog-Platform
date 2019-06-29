9a3f3fc5fdac037c276aaa4d3392ea6112093ef9
charles-m-doan <cmatthewdoan@gmail.com>
2019-06-29 13:35:37 -0400

    Pushing updated Post Controller.

--- src/main/java/wcci/BlogPlatform/controllers/PostController.java
+++ src/main/java/wcci/BlogPlatform/controllers/PostController.java
@@ -5,7 +5,6 @@ import javax.annotation.Resource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import wcci.BlogPlatform.repos.PostCrudRepo;
@@ -18,29 +17,25 @@ public class PostController {
 	PostCrudRepo postRepo;
 
 	@RequestMapping("")
-	public String renderAllPosts(Model model)
-		{
+	public String renderAllPosts(Model model) {
 		model.addAttribute("allPostsModel", postRepo.findAll());
 		return "allPostsView";
-		}
+	}
 
 	@RequestMapping("{id}")
-	public String renderSinglePost(@PathVariable("id") Long id, Model model)
-		{
-		// model.addAttribute("singlePostModel", reviewRepo.findById(id).get());
+	public String renderSinglePost(@PathVariable("id") Long id, Model model) {
+		model.addAttribute("singlePostModel", postRepo.findById(id).get());
 		return "singlePostView";
-		}
+	}
 
 	@RequestMapping("new")
-	public String renderNewPost(Model model)
-		{
+	public String renderNewPost(Model model) {
 		return "newPostView";
-		}
+	}
 
 	@RequestMapping("add")
 	// @PostMapping("add")
-	public String addPost()
-		{
+	public String addPost() {
 		return "redirect:/posts";
-		}
+	}
 }
