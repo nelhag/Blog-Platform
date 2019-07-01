26ea0392c47559d7f9fb629b8a29ac8054f1c356
charles-m-doan <cmatthewdoan@gmail.com>
2019-07-01 05:27:32 -0400

    Finishing most of the functionality, though still missing the ability to input multiple authors and multiple tags at the same time when creating a post.

--- src/main/java/wcci/BlogPlatform/controllers/CategoryController.java
+++ src/main/java/wcci/BlogPlatform/controllers/CategoryController.java
@@ -4,8 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import wcci.BlogPlatform.models.Category;
 import wcci.BlogPlatform.repos.AuthorCrudRepo;
 import wcci.BlogPlatform.repos.CategoryCrudRepo;
 import wcci.BlogPlatform.repos.PostCrudRepo;
@@ -38,13 +40,27 @@ public class CategoryController {
 	public String renderSingleCategory(@PathVariable("id") Long id, Model model)
 		{
 		model.addAttribute("singleCategoryModel", categoryRepo.findById(id).get());
+		model.addAttribute("postsModel", categoryRepo.findById(id).get().getPosts());
 		return "singleCategoryView";
 		}
 
-	@RequestMapping("add")
-// @PostMapping("add")
-	public String addCategory()
+	@PostMapping("add")
+	public String addCategory(String name)
 		{
+		if (name == null)
+			{
+			return "redirect:/categories";
+			}
+		if (name.isEmpty())
+			{
+			return "redirect:/categories";
+			}
+		Category existingCategory = categoryRepo.findByName(name);
+		if (existingCategory == null)
+			{
+			Category categoryToAdd = new Category(name);
+			categoryRepo.save(categoryToAdd);
+			}
 		return "redirect:/categories";
 		}
 }
